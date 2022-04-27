0f09f1e31d86c912e32b268ffeb11cc86da9fcc9
Daryl Sonnier <daryl@chaosgamingpc.com>
2022-04-26 22:54:02 -0500

    Reset timer
    Reset the timer when refreshing the token to keep in sync with the back end.

--- src/api/refreshToken.js
+++ src/api/refreshToken.js
@@ -1,11 +1,15 @@
 const URL = "https://demoschool.edu:9000/refreshtoken";
 
 export async function refreshToken(){
+
     const response = await fetch(URL, {
+        body: '',
         method: 'Post',
         mode: 'cors',
         credentials: 'include',
     })
-    console.log(response.statusText);
+    console.log(response.status);
+    clearInterval(window.interval);
+    window.interval = setInterval(refreshToken, 295000);
     return;
 };
